ebe89127a3a9713d2a6c1c7fb3ed6c4e66046414
Sunny <sunny.mishra0389@gmail.com>
2017-10-31 10:43:23 +0530

    Cyclic dependency removed

--- ScraperCore/src/main/java/com/marsplay/scraper/AgentConfig.java
+++ ScraperCore/src/main/java/com/marsplay/scraper/AgentConfig.java
@@ -1,18 +1,12 @@
 package com.marsplay.scraper;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.Callable;
-import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.lang.StringUtils;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebDriver.Timeouts;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,11 +19,10 @@ import com.marsplay.scraper.lib.Constants;
 import com.marsplay.scraper.lib.Constants.Endsites;
 
 @Configuration
-public class AgentsConfig {
+public class AgentConfig {
 	private static final Logger LOGGER = LoggerFactory
-			.getLogger(AgentsConfig.class);
+			.getLogger(AgentConfig.class);
 	private Properties applicationProps = Constants.getApplicationProps();
-	private Properties businessProps = Constants.getBusinessProps();
 
 	@Autowired
 	@Qualifier("myntraAgent")
@@ -47,22 +40,6 @@ public class AgentsConfig {
 	@Qualifier("koovsAgent")
 	private Agent koovsAgent;
 	
-	@Bean("myntraWebDriver")
-	public WebDriver myntraWebDriver() throws IOException {
-		LOGGER.info("#### Initializing Myntra Chrome...");
-		ChromeOptions options = new ChromeOptions();
-		System.setProperty("webdriver.chrome.driver",
-				applicationProps.getProperty("chrome.driver.path"));
-		options.addArguments("--headless", "--disable-gpu",
-				"--window-size=1920,1520"); // 1280,1696	"--incognito",
-		WebDriver myntraDriver = new ChromeDriver(options);
-
-		Timeouts myntraTimeouts = myntraDriver.manage().timeouts();
-		myntraTimeouts.pageLoadTimeout(Long.parseLong(businessProps
-				.getProperty("common.page_load_timeout_seconds")),
-				TimeUnit.SECONDS);
-		return myntraDriver;
-	}
 	@Bean("agents")
 	public List<Callable<String>> getAgents() {
 		String activeAgentsStr = applicationProps.getProperty("scraper.agent.active");
---
+++ ScraperCore/src/main/java/com/marsplay/scraper/WebDriverAgentConfig.java
@@ -0,0 +1,50 @@
+package com.marsplay.scraper;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.lang.StringUtils;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriver.Timeouts;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.marsplay.scraper.agents.Agent;
+import com.marsplay.scraper.lib.Constants;
+import com.marsplay.scraper.lib.Constants.Endsites;
+
+@Configuration
+public class WebDriverAgentConfig {
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(WebDriverAgentConfig.class);
+	private Properties applicationProps = Constants.getApplicationProps();
+	private Properties businessProps = Constants.getBusinessProps();
+
+	@Bean("myntraWebDriver")
+	public WebDriver myntraWebDriver() throws IOException {
+		LOGGER.info("#### Initializing Myntra Chrome...");
+		ChromeOptions options = new ChromeOptions();
+		System.setProperty("webdriver.chrome.driver",
+				applicationProps.getProperty("chrome.driver.path"));
+		options.addArguments("--headless", "--disable-gpu",
+				"--window-size=1920,1520"); // 1280,1696	"--incognito",
+		WebDriver myntraDriver = new ChromeDriver(options);
+
+		Timeouts myntraTimeouts = myntraDriver.manage().timeouts();
+		myntraTimeouts.pageLoadTimeout(Long.parseLong(businessProps
+				.getProperty("common.page_load_timeout_seconds")),
+				TimeUnit.SECONDS);
+		return myntraDriver;
+	}
+}
